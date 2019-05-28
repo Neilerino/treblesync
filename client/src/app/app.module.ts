64ff795b7c14751626cdf8cf9eeb8c6600502802
Neil Wadden <neilwadden@Neils-MacBook-Pro.local>
2019-05-28 09:41:53 -0230

    Added in the register component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ButtonComponent } from './components/button/button.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ButtonComponent
+    ButtonComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
