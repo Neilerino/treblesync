035d211b59a0f18257b4c6c2a0728c7cc10218c4
Neil Wadden <naw218@mun.ca>
2019-05-28 10:02:52 -0230

    Added the alert component, to help scaffold the register component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ButtonComponent } from './components/button/button.component';
 import { RegisterComponent } from './components/register/register.component';
+import { AlertComponent } from './_directives/alert/alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ButtonComponent,
-    RegisterComponent
+    RegisterComponent,
+    AlertComponent
   ],
   imports: [
     BrowserModule,
